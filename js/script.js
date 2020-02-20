3f7bbdcb3b972fc827a056c2ed58da8e0e84c0a1
killStrikeFF <43044956+killStrikeFF@users.noreply.github.com>
2020-02-21 01:21:49 +0300

    End of project

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    var mySwiper = new Swiper ('.swiper-container', {
-        // Optional parameters
+window.addEventListener('DOMContentLoaded', () => {
+
+    /* banner slider */
+    var mySwiper = new Swiper ('.swiper-container-baner', {
         loop: true,
     
-        // If we need pagination
         pagination: {
           el: '.swiper-pagination',
           type: 'bullets',
@@ -11,7 +11,9 @@ document.addEventListener('DOMContentLoaded', () => {
         },
     
     });
-    
+
+
+    /* fade catalog products */
     let btnProduct = document.querySelector('#btn-products'),
         products = document.querySelector('.main-products-block-content-ul');
     
@@ -24,5 +26,72 @@ document.addEventListener('DOMContentLoaded', () => {
 
     });
 
+
+    /* main plus slider*/
+    let tab = document.querySelectorAll('.info-header-tab'),        
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+            tab[i].style.color = '#525151';
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if(tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if(target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if(target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    tab[i].style.color = '#fa3f3e';
+                    break;
+                }
+            }
+        }
+    });
+
+    let widthScreen = document.documentElement.clientWidth,
+        colOfslides = 4;
+
+    console.log(widthScreen);
+
+    if (widthScreen > 1100 ) {
+        colOfslides = 4
+    } else if (widthScreen > 700 && widthScreen < 1100) {
+        colOfslides = 3
+    } else if (widthScreen > 0 && widthScreen < 700) {
+        colOfslides = 2
+    }
+
+    /* slider products */
+    var mySwiper = new Swiper ('.swiper-container-products', {
+        loop: true,
+        slidesPerView: colOfslides,
+        spaceBetween: 10,
+
+        
+
+        navigation: {
+          nextEl: '.products-button-next',
+          prevEl: '.products-button-prev',
+        },
+
+      })
+
+      
 
 });
